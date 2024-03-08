a60e629395ed1e4c74971b07987316ffe014d88e
locolin1204 <locolin99@gmail.com>
2024-03-08 13:54:27 +0800

    Added DynamoDB Config

--- src/main/kotlin/com/ninekicks/microservices/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/ninekicks/microservices/config/DynamoDBConfig.kt
@@ -1,7 +1,9 @@
 package com.ninekicks.microservices.config
 
+import aws.sdk.kotlin.runtime.auth.credentials.EnvironmentCredentialsProvider
 import aws.sdk.kotlin.runtime.auth.credentials.StaticCredentialsProvider
 import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,20 +11,19 @@ import org.springframework.context.annotation.Scope
 
 @Configuration
 class DynamoDBConfig {
-//    @Bean
-//    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-//    fun dynamoDbClientBuilderLocal(): DynamoDbClient.Builder {
-//
-//        val builder: DynamoDbClient.Builder = DynamoDbClient.builder()
-//
-//        builder.apply {
-//            this.config.region = "ap-southeast-1"
-//            // Add fake accessKey for local profile
-//            this.config.credentialsProvider = StaticCredentialsProvider {
-//                accessKeyId = ""
-//                secretAccessKey = ""
-//            }
-//        }
-//        return builder
-//    }
+    @Value("\${cloud.aws.credentials.accessKey}")
+    private val accessKey: String? = null
+    @Value("\${cloud.aws.credentials.secretAccessKey}")
+    private val secretKey: String? = null
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    fun dynamoDbClient(): DynamoDbClient {
+        return DynamoDbClient {
+            region = "ap-southeast-1"
+            credentialsProvider = if(accessKey.isNullOrEmpty() || secretKey.isNullOrEmpty()) EnvironmentCredentialsProvider() else StaticCredentialsProvider {
+                accessKeyId = accessKey
+                secretAccessKey = secretKey
+            }
+        }
+    }
 }
