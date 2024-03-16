e814ca27d0f1315014ed4ce9713953be476c3911
felixfung203 <felixlam203@gmail.com>
2024-03-17 02:03:59 +0800

    feat: Created Product Detail and Update Converter Detail (+2 squashed commit)
    Squashed commit:
    
    [d9bfece] feat: update converter
    
    [3a91666] feat: Created Product Detail and change converter (+1 squashed commits)
    
    Squashed commits:
    
    [43f59a5] feat: Created Product Detail

---
+++ src/main/kotlin/com/ninekicks/microservices/model/Product.kt
@@ -0,0 +1,41 @@
+package com.ninekicks.microservices.model
+
+import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
+import com.amazonaws.services.dynamodbv2.datamodeling.*
+import com.ninekicks.microservices.helper.ListConverter
+import com.ninekicks.microservices.helper.MapConverter
+
+@DynamoDBTable(tableName ="9Kicks")
+data class Product(
+        @DynamoDBHashKey(attributeName = "PK")
+        var productId:String,
+        @DynamoDBRangeKey(attributeName = "SK")
+        private val sortKey:String = "PRODUCT_DETAIL",
+        @DynamoDBAttribute(attributeName = "productName")
+        var productName:String,
+        @DynamoDBAttribute(attributeName = "productBrand")
+        var productBrand:String,
+        @DynamoDBAttribute(attributeName = "productCategory")
+        var productCategory:String,
+        @DynamoDBAttribute(attributeName = "productSlug")
+        var productSlug:String,
+        @DynamoDBAttribute(attributeName = "imageUrl")
+        var imageUrl:String,
+        @DynamoDBAttribute(attributeName = "price")
+        var price: Float,
+        @DynamoDBAttribute(attributeName = "isDiscount")
+        var isDiscount:Boolean,
+        @DynamoDBAttribute(attributeName = "discountPrice")
+        var discountPrice: Float?,
+        @DynamoDBAttribute(attributeName = "detailImageUrl")
+        @DynamoDBTypeConverted(converter = ListConverter::class)
+        var detailImageUrl: List<String>,
+        @DynamoDBAttribute(attributeName = "productSize")
+        @DynamoDBTypeConverted(converter = MapConverter::class)
+        var productSize: Map<String, Int>
+) {
+}
+
+
+
+
