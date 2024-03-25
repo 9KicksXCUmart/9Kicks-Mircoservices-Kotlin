1cd51396d71680390b0bc026316e4bcda7a50fee
locolin1204 <locolin99@gmail.com>
2024-03-25 15:31:35 +0800

    fix: fix merge conflict

--- src/main/kotlin/com/ninekicks/microservices/service/impl/AccountSummaryServiceImpl.kt
+++ src/main/kotlin/com/ninekicks/microservices/service/impl/AccountSummaryServiceImpl.kt
@@ -65,37 +65,6 @@ class AccountSummaryServiceImpl(
         }
     }
 
-    override fun listOrdersByUserId(
-        userId: String,
-        pageSize: Int,
-        lastKey: Map<String, AttributeValue>?
-    ): ResponseEntity<Any> {
-        return runBlocking {
-            val orders = orderRepository.getOrdersByUserId(userId, pageSize, lastKey)
-            val orderDetailDtoList = orders?.map {
-                OrderDetailDTO(
-                    userId= it.userId,
-                    orderId = it.orderId,
-                    orderStatus = it.orderStatus,
-                    deliveryStatus = it.deliveryStatus,
-                    orderDate = it.orderDate,
-                    receivedDate = it.receivedDate,
-                    orderItemDetail = it.orderItemDetail?.map { item ->
-                        OrderDetailDTO.OrderItemDetail(
-                            product = productRepository.getProductDetail(item.productId),
-                            sizeQuantity = item.sizeQuantity
-                        )
-                    },
-                    totalPrice = it.totalPrice
-                )
-            }
-            responseHandler.validateResponse(
-                failMessage = "No orders found",
-                matchingObject = orderDetailDtoList,
-                failReturnObject = emptyList<Order>()
-            )
-        }
-    }
     override fun displayOrderDetails(userId: String, orderId: String): ResponseEntity<Any> {
         TODO("Not yet implemented")
     }
